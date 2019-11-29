149cc6a54dd7b89eeca85746230b437df03c943e
jvdbergh <jvandenberghe@isabelgroup.eu>
2019-11-29 13:43:34 +0100

    replace scaffolding

--- src/test/java/be/joeri/tdd/FractionAdditionTest.java
+++ src/test/java/be/joeri/tdd/FractionAdditionTest.java
@@ -39,10 +39,8 @@ public class FractionAdditionTest {
 
 	@Test
 	public void nonTrivialButCommonDenominator() throws Exception {
-		Fraction sum = new Fraction(1,5).plus(new Fraction(2,5));
-		assertEquals(3, sum.getNumerator());
-		assertEquals(5, sum.getDenominator());
+		assertEquals(
+				new Fraction(3, 5),
+				new Fraction(1,5).plus(new Fraction(2,5)));
 	}
-
-
 }
