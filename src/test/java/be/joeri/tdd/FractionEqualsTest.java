66222ab41138c4786a58fcc9c93ce40ede9c7d6f
jvdbergh <jvandenberghe@isabelgroup.eu>
2019-11-29 13:41:27 +0100

    enough tests added to equals

--- src/test/java/be/joeri/tdd/FractionEqualsTest.java
+++ src/test/java/be/joeri/tdd/FractionEqualsTest.java
@@ -1,6 +1,7 @@
 package be.joeri.tdd;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import be.joeri.tdd.model.Fraction;
 import org.junit.Test;
@@ -9,6 +10,25 @@ public class FractionEqualsTest {
 	@Test
 	public void sameNumeratorAndDenominator() throws Exception {
 		assertEquals(new Fraction(3,5), new Fraction(3,5));
+	}
 
+	@Test
+	public void differentNumerators() throws Exception {
+		assertNotEquals(new Fraction(3,5), new Fraction(4,5));
+	}
+
+	@Test
+	public void differentDenominators() throws Exception {
+		assertNotEquals(new Fraction(3,5), new Fraction(3,7));
+	}
+
+	@Test
+	public void wholeNumberEqualsSameFraction() throws Exception {
+		assertEquals(new Fraction(5,1), new Fraction(5));
+	}
+
+	@Test
+	public void nameWholeNumberNotEqualsToDifferentWholeNumber() throws Exception {
+		assertNotEquals(new Fraction(5), new Fraction(7));
 	}
 }
