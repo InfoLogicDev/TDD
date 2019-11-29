bc53452070e07cc75aef4c06011c12f811f0ba7f
jvdbergh <jvandenberghe@isabelgroup.eu>
2019-11-29 13:18:04 +0100

    no longer fake numerator

--- src/main/java/be/joeri/tdd/model/Fraction.java
+++ src/main/java/be/joeri/tdd/model/Fraction.java
@@ -3,19 +3,22 @@ package be.joeri.tdd.model;
 public class Fraction {
 	private final int denominator;
 	private final int integerValue;
+	private final int numerator;
 
 	public Fraction(Integer integerValue) {
 		this.integerValue = integerValue;
+		this.numerator = integerValue;
 		this.denominator = 1;
 	}
 
 	public Fraction(int numerator, int denominator) {
 		this.integerValue = numerator;
+		this.numerator = numerator;
 		this.denominator = denominator;
 	}
 
 	public int getNumerator() {
-		return 3;
+		return numerator;
 	}
 
 	public int getDenominator() {
