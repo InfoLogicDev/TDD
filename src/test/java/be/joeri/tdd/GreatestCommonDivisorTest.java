96893b7384728aa704a320ced6acb45b36c6c147
jvdbergh <jvandenberghe@isabelgroup.eu>
2019-11-29 14:34:33 +0100

    explore gcd algorithm

--- src/test/java/be/joeri/tdd/GreatestCommonDivisorTest.java
+++ src/test/java/be/joeri/tdd/GreatestCommonDivisorTest.java
@@ -6,8 +6,36 @@ import org.junit.Test;
 
 public class GreatestCommonDivisorTest {
 	@Test
-	public void oneAndOne() throws Exception {
+	public void reflexive() throws Exception {
 		assertEquals(1, gcd(1,1));
+		assertEquals(2, gcd(2,2));
+		assertEquals(-1, gcd(-1,-1));
+	}
+
+	@Test
+	public void relativePrime() throws Exception {
+		assertEquals(1, gcd(2,3));
+		assertEquals(1, gcd(4,7));
+		assertEquals(-1, gcd(-2,-3));
+	}
+
+	@Test
+	public void oneIsMultipleOfTheOther() throws Exception {
+		assertEquals(3, gcd(3,9));
+		assertEquals(5, gcd(5,30));
+	}
+
+	@Test
+	public void commonFactor() throws Exception {
+		assertEquals(2, gcd(6,8));
+		assertEquals(7, gcd(49,315));
+	}
+
+	@Test
+	public void negatives() throws Exception {
+		assertEquals(-4, gcd(-24,-28));
+		assertEquals(4, gcd(-24,28));
+		assertEquals(-4, gcd(24,-28));
 	}
 
 	private int gcd(int a, int b) {
